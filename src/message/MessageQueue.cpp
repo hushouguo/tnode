2cfb7594240ae19f8fe13d78e44230391f198dee
hushouguo <kilimajaro@gmail.com>
2019-04-02 17:34:48 +0800

    MessageQueue

---
+++ src/message/MessageQueue.cpp
@@ -0,0 +1,10 @@
+/*
+ * \file: MessageQueue.cpp
+ * \brief: Created by hushouguo at 17:34:13 Apr 02 2019
+ */
+
+#include "tnode.h"
+
+BEGIN_NAMESPACE_TNODE {
+}
+
---
+++ src/message/MessageQueue.h
@@ -0,0 +1,12 @@
+/*
+ * \file: MessageQueue.h
+ * \brief: Created by hushouguo at 17:34:01 Apr 02 2019
+ */
+ 
+#ifndef __MESSAGEQUEUE_H__
+#define __MESSAGEQUEUE_H__
+
+BEGIN_NAMESPACE_TNODE {
+}
+
+#endif
