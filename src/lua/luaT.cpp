bd5e621866253f45f273a6ff2779b33fc0798dd8
hushouguo <kilimajaro@gmail.com>
2019-04-04 15:01:11 +0800

    lua aux

---
+++ src/lua/luaT.cpp
@@ -0,0 +1,10 @@
+/*
+ * \file: luaT.cpp
+ * \brief: Created by hushouguo at 15:00:47 Apr 04 2019
+ */
+
+#include "tnode.h"
+
+BEGIN_NAMESPACE_TNODE {
+}
+
---
+++ src/lua/luaT.h
@@ -0,0 +1,12 @@
+/*
+ * \file: luaT.h
+ * \brief: Created by hushouguo at 15:00:36 Apr 04 2019
+ */
+ 
+#ifndef __LUAT_H__
+#define __LUAT_H__
+
+BEGIN_NAMESPACE_TNODE {
+}
+
+#endif
